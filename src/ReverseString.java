300fe590209a9a328ab2a702d28a221b31059d21
Derrick <derricknjeru5@gmail.com>
2020-03-08 09:56:55 +0300

    edited ReverseString question

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -6,10 +6,23 @@ public class ReverseString {
 
     /**
      * Write a function that reverses a string. The input string is given as an array of characters char[].
-     * <p>
+     *
      * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
-     * <p>
+     *
      * You may assume all the characters consist of printable ascii characters.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: ["h","e","l","l","o"]
+     * Output: ["o","l","l","e","h"]
+     * Example 2:
+     *
+     * Input: ["H","a","n","n","a","h"]
+     * Output: ["h","a","n","n","a","H"]
+     * Hide Hint #1
+     * The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
      */
     static String reverseString(char[] s) {
         int i = 0; //first index
