382575bfea17448fa9845f9986a28964100c872c
Derrick <derricknjeru5@gmail.com>
2020-03-10 07:24:33 +0300

    Refreshed on Linked list

--- src/FirstUniqueCharacter.java
+++ src/FirstUniqueCharacter.java
@@ -1,7 +1,4 @@
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 public class FirstUniqueCharacter {
     public static void main(String[] args) {
---
+++ src/practise/LinkedListTraversal.java
@@ -0,0 +1,40 @@
+package practise;
+
+public class LinkedListTraversal {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    /* Linked list Node.  This inner class is made static so that
+          main() can access it */
+    public static void main(String[] args) {
+        LinkedListTraversal llist = new LinkedListTraversal();
+
+        llist.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        llist.head.next = second; // Link first node with the second node
+        second.next = third;// Link first node with the second node
+
+        // Link first node with the second node
+        llist.printList();
+    }
+
+    /* This function prints contents of linked list starting from head */
+    public void printList() {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+}
