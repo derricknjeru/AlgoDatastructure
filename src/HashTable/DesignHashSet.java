3d53dea9ee705594d54f20a6e0d96f7610d49110
derricknjeru <derricknjeru5@gmail.com>
2022-04-29 12:48:45 +0300

    design hashtable

---
+++ src/HashTable/DesignHashSet.java
@@ -0,0 +1,48 @@
+package HashTable;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DesignHashSet {
+    private int numBuckets = 15000;
+    LinkedList[] buckets;
+
+    private int hash_function(int key) {
+        return key % numBuckets;
+    }
+
+    public DesignHashSet() {
+        buckets = new LinkedList[numBuckets];
+    }
+
+    public void add(int key) {
+        int i = hash_function(key);
+        //create a new list
+        if (buckets[i] == null) {
+            buckets[i] = new LinkedList<>();
+        }
+
+        if (buckets[i].indexOf(key) == -1) {
+            buckets[i].add(key);
+        }
+    }
+
+    public void remove(int key) {
+        int i = hash_function(key);
+        //create a new list
+        if (buckets[i] == null) return;
+
+        if (buckets[i].indexOf(key) != -1) {
+            buckets[i].remove(buckets[i].indexOf(key));
+        }
+    }
+
+    public boolean contains(int key) {
+        int i = hash_function(key);
+        //create a new list
+        if (buckets[i] == null || buckets[i].indexOf(key) == -1) {
+            return false;
+        }
+        return true;
+    }
+}
