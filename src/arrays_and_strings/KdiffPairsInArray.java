c62f075d1045585c74b568bd999016e8bb2758f8
derricknjeru <derricknjeru5@gmail.com>
2022-06-20 23:29:12 +0300

    Find pair soln

---
+++ src/arrays_and_strings/KdiffPairsInArray.java
@@ -0,0 +1,65 @@
+package arrays_and_strings;
+
+import java.util.HashMap;
+
+public class KdiffPairsInArray {
+    /**
+     * Given an array of integers nums and an integer k, return the number of unique k-diff pairs in the array.
+     * <p>
+     * A k-diff pair is an integer pair (nums[i], nums[j]), where the following are true:
+     * <p>
+     * 0 <= i, j < nums.length
+     * i != j
+     * nums[i] - nums[j] == k
+     * Notice that |val| denotes the absolute value of val.
+     * <p>
+     * <p>
+     * <p>
+     * Example 1:
+     * <p>
+     * Input: nums = [3,1,4,1,5], k = 2
+     * Output: 2
+     * Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+     * Although we have two 1s in the input, we should only return the number of unique pairs.
+     * Example 2:
+     * <p>
+     * Input: nums = [1,2,3,4,5], k = 1
+     * Output: 4
+     * Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+     * Example 3:
+     * <p>
+     * Input: nums = [1,3,1,5,4], k = 0
+     * Output: 1
+     * Explanation: There is one 0-diff pair in the array, (1, 1).
+     * <p>
+     * <p>
+     * Constraints:
+     * <p>
+     * 1 <= nums.length <= 104
+     * -107 <= nums[i] <= 107
+     * 0 <= k <= 107
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findPairs(int[] nums, int k) {
+        HashMap<Integer, Integer> occurencesMap = new HashMap<>();
+        for (int num : nums) {
+            occurencesMap.put(num, occurencesMap.getOrDefault(num, 0) + 1);
+        }
+
+        int pair = 0;
+        for (int n : occurencesMap.keySet()) {
+            //There is a value that can be - from k
+            if (k > 0 && occurencesMap.containsKey(n + k)) pair++;
+
+            //
+            if (k == 0 && occurencesMap.get(n) > 1) pair++;
+
+        }
+
+        return pair;
+
+    }
+}
