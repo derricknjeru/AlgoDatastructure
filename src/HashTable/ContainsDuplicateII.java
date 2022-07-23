0fc448067e0ef4d644180e4d8345bbf14bd4726e
derricknjeru <derricknjeru5@gmail.com>
2022-07-23 11:52:39 +0300

    added a soln for duplicates

--- src/HashTable/ContainsDuplicateII.java
+++ src/HashTable/ContainsDuplicateII.java
@@ -1,20 +1,22 @@
 package HashTable;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicateII {
     //https://leetcode.com/problems/contains-duplicate-ii/
+    //https://www.youtube.com/watch?v=SFMCxqSeM94&t=934s
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int i =0; i<nums.length; i++){
+        Set<Integer> set = new HashSet<>();
+        //time complexity = O(n)
+        //space complexity O(min(n,k));
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.add(nums[i])) return true;
 
-            int current = nums[i];
-
-            if(map.containsKey(current) && i-map.get(current) <=k){
-                return true;
-            }else{
-                map.put(current,i);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
             }
         }
 
---
+++ src/RevisionGoogle/top_10/TextJustification.java
@@ -0,0 +1,12 @@
+package RevisionGoogle.top_10;
+
+public class TextJustification {
+    //https://leetcode.com/problems/text-justification/
+    //Other questions ->
+    //https://www.youtube.com/watch?v=AWezsCJDHSk
+    //https://www.youtube.com/watch?v=L2QR_w5NlWU
+
+    //soln
+    //https://www.youtube.com/watch?v=GqXlEbFVTXY
+    //https://www.youtube.com/watch?v=RORuwHiblPc&t=19s
+}
