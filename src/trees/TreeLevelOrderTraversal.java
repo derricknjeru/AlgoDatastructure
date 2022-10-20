cb5846b946b0d9c54e93178ac92ac45b6cb606a2
Derrick <derrick@Derrick-Mac.local>
2022-10-20 23:36:23 +0300

    updated level order

--- src/trees/TreeLevelOrderTraversal.java
+++ src/trees/TreeLevelOrderTraversal.java
@@ -4,6 +4,7 @@ package trees;
 import java.util.*;
 
 class TreeLevelOrderTraversal {
+    //https://www.hackerrank.com/challenges/tree-level-order-traversal/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
     static class Node {
         Node left;
         Node right;
